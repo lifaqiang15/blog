44eca71d8ae3b97a60ded379fe80197eed5ab8f0
lifaqiang15 <lifaqiang15@163.com>
2025-05-28 23:33:22 +0800

    feat: 新建vue3+element plus项目

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-config-provider :locale="zhCn">
+    <div style="background-color: red">
+      <el-button @click="fun">Button</el-button>
+    </div>
+  </el-config-provider>
+</template>
+
+<script setup lang="ts">
+import { ElConfigProvider } from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+const fun = () => {
+  ElMessage.success('error')
+}
+</script>
+
+<style scoped lang="scss"></style>
---
+++ frontend/vite.config.ts
@@ -0,0 +1,34 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+// https://vite.dev/config/
+export default defineConfig(() => {
+  return {
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver({ importStyle: 'sass' })],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver({ importStyle: 'sass' })],
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "@/styles/index.scss" as *;`,
+        },
+      },
+    },
+  }
+})

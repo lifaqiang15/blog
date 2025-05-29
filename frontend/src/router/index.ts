c556981b9bbeddb42cd703e5272b0d24ac554bda
lifaqiang15 <lifaqiang15@163.com>
2025-05-29 22:49:50 +0800

    feat: 添加路由组件和路由配置

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,41 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('@/views/HomeView.vue'),
+  },
+  {
+    path: '/article/:id',
+    name: 'article',
+    component: () => import('@/views/BlogView.vue'),
+    meta: { title: '文章详情' },
+    props: true,
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'notfound',
+    component: () => import('@/views/NotFound.vue'),
+    meta: { title: '页面不存在' },
+  },
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  },
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title} | 个人博客`
+  next()
 })
 
 export default router
---
+++ frontend/src/views/BlogView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="blog-view">{{ id }}</div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps(['id'])
+</script>
+
+<style scoped lang="scss"></style>
